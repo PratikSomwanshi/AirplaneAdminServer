8a18f04631df1c2eefe7d56b8dcbf29ccfb2e2aa
Pratik Somwanshi <pratiksomwanshi570@gmail.com>
2024-11-23 11:40:18 +0530

    done with basic Airplane Admin Server

---
+++ src/test/java/com/wanda/AirplaneAdminServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.wanda;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AirplaneAdminServerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
